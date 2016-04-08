60dabfa28d0fd363a745a86f8245847c1d8178b3
birneysky <birneysky@163.com>
2016-04-08 15:32:17 +0800

    modfiy

--- archive_ipa/AIConfigureWorker.m
+++ archive_ipa/AIConfigureWorker.m
@@ -58,6 +58,13 @@
     //[self setPlistFileValue:self.configInfo.bundleIdentifier fromKey:@"CFBundleIdentifier" filePath:self.configInfo.appInfoListPath];
 }
 
+- (void)configProvisioningProfile
+{
+    NSString* commandFromat = @"perl -p -i -e \"s/PROVISIONING_PROFILE = *.*/PROVISIONING_PROFILE = \\\"\\\";/g\" %@/project.pbxproj";
+    NSString* command = [NSString stringWithFormat:commandFromat,self.configInfo.appProjectPath];
+    [self.executor executeCommand:command];
+}
+
 - (void)configTranslateAppName
 {
     NSDictionary* appNames = self.configInfo.appName;
@@ -131,12 +138,46 @@
 
 - (void)produceLaunchImage
 {
-    
+    NSString* temPicture = @"temp.png";
+    NSString* iconPicture = @"iconTemp.png";
+    CGSize sizes[6] = {CGSizeMake(640, 960),CGSizeMake(640, 1136),CGSizeMake(750, 1334),CGSizeMake(1024, 768),CGSizeMake(1242, 2208),CGSizeMake(2048, 1536)};
+    for(int i = 0; i < 6; i++){
+        CGSize size = sizes[i];
+        NSString* resizeCommandFormat = @"convert -resize %.fx%.f! %@ %@";
+        NSString* resizeCommand = [NSString stringWithFormat:resizeCommandFormat,size.width,size.height,self
+                                   .configInfo.compositeLogoPath,self.configInfo.compositeLogoPath];
+        [self.executor executeCommand:resizeCommand];
+        //NSLog(@"produceLaunchImage %@",resizeCommand);
+        
+        CGFloat iconWidth =  rintf(sqrt(size.width * size.width + size.height * size.height) * 0.03);
+        NSString* resizeIconCommand = [NSString stringWithFormat:resizeCommandFormat,iconWidth,iconWidth,self.configInfo.appIconSrcPath,iconPicture];
+        [self.executor executeCommand:resizeIconCommand];
+        
+        NSString* drawIconCommandFormat = @"composite -gravity south  -geometry -%.f+50 %@ %@ %@";
+        //NSString* iconPath = [self.configInfo.appIconDstPath stringByAppendingPathComponent:@"29x29.png"];
+        NSString* drawIconCommand = [NSString stringWithFormat:drawIconCommandFormat,iconWidth,iconPicture,self.configInfo.compositeLogoPath,temPicture];
+        //NSLog(@"produceLaunchImage %@",drawIconCommand);
+        [self.executor executeCommand:drawIconCommand];
+        
+        
+        //NSLog(@"width = %f",iconWidth);
+        
+        NSString* drawTextCommandFormat = @"convert %@ -fill gray  -font Times-Bold -pointsize %.f -gravity south -draw \"text %.f,48 '%@'\" %@/%.fx%.f.png";
+        NSString* drawTextCommand = [NSString stringWithFormat:drawTextCommandFormat,temPicture,iconWidth,iconWidth + 10,self.configInfo.version,self.configInfo.appLaunchImagePath,size.width,size.height];
+        [self.executor executeCommand:drawTextCommand];
+        //NSLog(@"produceLaunchImage %@",drawText);
+    }
+    [self.executor executeCommand:[NSString stringWithFormat:@"rm %@",temPicture]];
+    [self.executor executeCommand:[NSString stringWithFormat:@"rm %@",iconPicture]];
+    [self.executor executeCommand:[NSString stringWithFormat:@"convert -resize %.fx%.f! %@ %@",256.0f,256.0f,self.configInfo.compositeLogoPath,self.configInfo.compositeLogoPath]];
 }
 
 - (void)setBundleDisplayName
 {
     [self setPlistFileValue:self.configInfo.bundleDisplayName fromKey:@"CFBundleDisplayName" filePath:self.configInfo.appInfoListPath];
+    [self setPlistFileValue:self.configInfo.version fromKey:@"CFBundleVersion" filePath:self.configInfo.appInfoListPath];
+    [self setPlistFileValue:self.configInfo.version fromKey:@"CFBundleShortVersionString" filePath:self.configInfo.appInfoListPath];
+
 }
 
 - (void)configServerAddress
@@ -150,6 +191,10 @@
     [self setPlistFileValue:self.configInfo.copyright fromKey:@"Copyright" filePath:self.configInfo.appConfigureListPath];
     [self setPlistFileValue:self.configInfo.appName fromKey:@"AppName" filePath:self.configInfo.appConfigureListPath];
     [self setPlistFileValue:@(self.configInfo.visitorLoginEnable) fromKey:@"VisitorLoginEnable" filePath:self.configInfo.appConfigureListPath];
+    [self setPlistFileValue:@(self.configInfo.guidePageEnable) fromKey:@"GuidePageEnable" filePath:self.configInfo.appConfigureListPath];
+    if (self.configInfo.updateURL.length > 0) {
+        [self setPlistFileValue:self.configInfo.updateURL fromKey:@"UpdateURL" filePath:self.configInfo.appConfigureListPath];
+    }
 }
 
 - (void)configPushkey
@@ -157,16 +202,43 @@
     [self setPlistFileValue:self.configInfo.pushAppKey fromKey:@"APP_KEY" filePath:self.configInfo.appPushConfigListPath];
 }
 
+- (void)copyGuidePage
+{
+    if (!self.configInfo.guidePageEnable) {
+        return;
+    }
+    for (int i = 0; i < 4; i++) {
+        NSString* srcIphoneFileName = [NSString stringWithFormat:@"iPhone_Page%d.png",i+1];
+        NSString* srcIphonePath = [self.configInfo.appGuidePageSrcPath stringByAppendingPathComponent:srcIphoneFileName];
+        NSString* dstIphoneDictoryName = [NSString stringWithFormat:@"iPhone_Page%d.imageset",i+1];
+        NSString* dstIphonePath = [self.configInfo.appGuidePageDstPath stringByAppendingPathComponent:dstIphoneDictoryName];
+        
+        NSString* copyCommandForIPhone = [NSString stringWithFormat:@"cp %@ %@",srcIphonePath,dstIphonePath];
+        //NSLog(@"copyGuidePage %@",copyCommandForIPhone);
+        [self.executor executeCommand:copyCommandForIPhone];
+        
+        NSString* srcIpadFileName = [NSString stringWithFormat:@"iPad_Page%d.png",i+1];
+        NSString* srcIpadPath = [self.configInfo.appGuidePageSrcPath stringByAppendingPathComponent:srcIpadFileName];
+        NSString* dstIpadDictoryName = [NSString stringWithFormat:@"iPad_Page%d.imageset",i+1];
+        NSString* dstIpadPath = [self.configInfo.appGuidePageDstPath stringByAppendingPathComponent:dstIpadDictoryName];
+        NSString* copyCommandForIPad = [NSString stringWithFormat:@"cp %@ %@",srcIpadPath,dstIpadPath];
+        //NSLog(@"copyGuidePage %@",copyCommandForIPad);
+        [self.executor executeCommand:copyCommandForIPad];
+    }
+}
 
 - (BOOL)configure
 {
     [self configBundleID];
+    [self configProvisioningProfile];
     [self setBundleDisplayName];
     [self configServerAddress];
     [self configCopyRightText];
     [self configPushkey];
     [self configTranslateAppName];
     [self produceAppIcon];
+    [self produceLaunchImage];
+    [self copyGuidePage];
     return YES;
 }
 
